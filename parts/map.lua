fcf28ba07344692e93a3d658891a319aba312496
MrZ626 <1046101471@qq.com>
2021-12-17 18:39:08 +0800

    谱面加载失败会显示正确行号 整理代码，报错消息更好看

--- parts/map.lua
+++ parts/map.lua
@@ -1,6 +1,5 @@
 local int,rnd=math.floor,math.random
 local ins,rem=table.insert,table.remove
-local assert=assert
 
 local Map={}
 
@@ -14,18 +13,10 @@ local mapInfoKeys={
     "tracks",
 }
 
-local _iter
-local function iterator()
-    while true do
-        local l=_iter()
-        if l then
-            l=STRING.trim(l)
-            if #l>0 then
-                return l
-            end
-        else
-            return
-        end
+local SCline,SCstr
+local function _syntaxCheck(cond,msg)
+    if not cond then
+        error(("[$1] $2: $3"):repD(SCline,SCstr,msg))
     end
 end
 
@@ -36,7 +27,7 @@ function Map.new(file)
         musicAuth='[musicAuth]',
         mapAuth='[mapAuth]',
 
-        songFile="[songFile]",
+        songFile="songFile]",
         songOffset=0,
         tracks=4,
 
@@ -47,56 +38,64 @@ function Map.new(file)
         finished=false,
     }
 
-    _iter=love.filesystem.lines(file)
+    --Read file
+    local fileData={}do
+        local lineNum=1
+        for l in love.filesystem.lines(file)do
+            l=STRING.trim(l)
+            if l~=""then
+                ins(fileData,{lineNum,l})
+            end
+            lineNum=lineNum+1
+        end
+    end
 
-    local l=iterator()
-
-    --Read metadata
+    --Load metadata
     while true do
-        if l:sub(1,1)=='$'then
-            local k=l:sub(2,l:find("=")-1)
-            assert(TABLE.find(mapInfoKeys,k),"Invalid map info key:7 "..l)
-            o[k]=l:sub(assert(l:find("="),"Syntax error: need '='")+1)
-            l=iterator()
+        local str=fileData[1][2]
+        SCline,SCstr=fileData[1][1],str
+        if str:sub(1,1)=='$'then
+            local k=str:sub(2,str:find("=")-1)
+            _syntaxCheck(TABLE.find(mapInfoKeys,k),"Invalid map info key '"..k.."'")
+            _syntaxCheck(str:find("="),"Syntax error (need '=')")
+            o[k]=str:sub(str:find("=")+1)
+            rem(fileData,1)
         else
             break
         end
     end
 
+    --Parse non-string metadata
     if type(o.tracks)=='string'then o.tracks=tonumber(o.tracks)end
     if type(o.songOffset)=='string'then o.songOffset=tonumber(o.songOffset)end
-
-    repeat
-        ins(o.eventQueue,l)
-        l=iterator()
-    until not l
 
     --Parse notes & animations
     local curTime,curBPM=0,180
     local loopMark,loopEnd,loopCountDown
     local longBarState=TABLE.new(false,o.tracks)
     local lastLineState=TABLE.new(false,o.tracks)
-    local line=#o.eventQueue
+    local line=#fileData
     while line>0 do
-        local str=o.eventQueue[line]
-        local str0=str--Original string, for error message
+        local str=fileData[line][2]
+
+        SCline,SCstr=fileData[line][1],str--For assertion
 
         if str:sub(1,1)=='#'then--Annotation
             --Do nothing
         elseif str:sub(1,1)=='!'then--BPM mark
             local bpm=tonumber(str:sub(2))
-            assert(type(bpm)=='number'and bpm>0,"[Invalid BPM mark] "..str0)
+            _syntaxCheck(type(bpm)=='number'and bpm>0,"Invalid BPM mark")
             curBPM=bpm
         elseif str:sub(1,1)==':'then--Time mark
-            assert(not loopMark,"[Cannot set time in loop] "..str0)
+            _syntaxCheck(not loopMark,"Cannot set time in loop")
 
             str=str:sub(2)
             local stamp=STRING.split(str,":")
 
-            assert(#stamp==2 and type(stamp[1])=='number'and type(stamp[2])=='number',"[Wrong Time stamp] "..str0)
+            _syntaxCheck(#stamp==2 and type(stamp[1])=='number'and type(stamp[2])=='number',"Wrong Time stamp")
 
             stamp=tonumber(stamp[1])*60+tonumber(stamp[2])
-            assert(stamp>curTime,"[Cannot warp to past] "..str0)
+            _syntaxCheck(stamp>curTime,"Cannot warp to past")
 
             curTime=stamp
         elseif str:sub(1,1)=='='then--Repeat mark
@@ -105,20 +104,20 @@ function Map.new(file)
                 len=len+1
                 str=str:sub(2)
             until str:sub(1,1)~='='
-            assert(len>=4 and len<=10,"[Invalid repeat mark length] "..str0)
+            _syntaxCheck(len>=4 and len<=10,"Invalid repeat mark length")
 
             if str:sub(1,1)=='S'then
-                assert(not loopMark,"[Cannot start another loop in a loop] "..str0)
+                _syntaxCheck(not loopMark,"Cannot start another loop in a loop")
                 loopMark=line
                 if str:sub(2)==''then
                     loopCountDown=1
                 else
                     loopCountDown=tonumber(str:sub(2))
-                    assert(loopCountDown>=2 and int(loopCountDown)==loopCountDown,"[Invalid loop count] "..str0)
+                    _syntaxCheck(loopCountDown>=2 and int(loopCountDown)==loopCountDown,"Invalid loop count")
                     loopCountDown=loopCountDown-1
                 end
             elseif str=='E'then
-                assert(loopMark,"[Cannot end a loop without start one] "..str0)
+                _syntaxCheck(loopMark,"Cannot end a loop without start one")
                 if loopCountDown>0 then
                     loopEnd=line
                     loopCountDown=loopCountDown-1
@@ -134,11 +133,11 @@ function Map.new(file)
                     line=loopEnd
                 end
             else
-                error("[Invalid repeat mark] "..str0)
+                _syntaxCheck(false,"Invalid repeat mark")
             end
         else--Notes
             local readState='note'
-            local curLineState=TABLE.new(false,o.tracks)
+            local trackUsed=TABLE.new(false,o.tracks)
             local curTrack=1
             local step=1
 
@@ -154,7 +153,7 @@ function Map.new(file)
                                 track=curTrack,
                             })
                         elseif c=='U'then--Long bar start
-                            assert(not longBarState[curTrack],"[Cannot start a long bar in a long bar] "..str0)
+                            _syntaxCheck(not longBarState[curTrack],"Cannot start a long bar in a long bar")
                             local b={
                                 type="bar",
                                 track=curTrack,
@@ -165,22 +164,22 @@ function Map.new(file)
                             ins(o.eventQueue,b)
                             longBarState[curTrack]=b
                         elseif c=='A'then--Long bar stop
-                            assert(longBarState[curTrack],"[No long bar to stop] "..str0)
+                            _syntaxCheck(longBarState[curTrack],"No long bar to stop")
                             longBarState[curTrack].etime=curTime
                             longBarState[curTrack].tail=true
                             longBarState[curTrack]=false
                         elseif c=='H'then--Long bar end
-                            assert(longBarState[curTrack],"[No long bar to end] "..str0)
+                            _syntaxCheck(longBarState[curTrack],"No long bar to end")
                             longBarState[curTrack].etime=curTime
                             longBarState[curTrack]=false
                         else
-                            assert(curTrack==o.tracks+1,"[Too few notes in one line] "..str0)
+                            _syntaxCheck(curTrack==o.tracks+1,"Too few notes in one line")
                             readState='rnd'
                             goto CONTINUE_nextState
                         end
-                        curLineState[curTrack]=true
+                        trackUsed[curTrack]=true
                     end
-                    assert(curTrack<=o.tracks,"[Too many notes in one line] "..str0)
+                    _syntaxCheck(curTrack<=o.tracks,"Too many notes in one line")
                     curTrack=curTrack+1
                     str=str:sub(2)
                 elseif readState=='rnd'then
@@ -188,7 +187,7 @@ function Map.new(file)
                     local available={}
                     for i=1,o.tracks do available[i]=i end
                     for i=#available,1,-1 do
-                        if curLineState[available[i]]then
+                        if trackUsed[available[i]]then
                             rem(available,i)
                         end
                     end
@@ -197,13 +196,13 @@ function Map.new(file)
                     c=c:upper()
                     if c=='L'then--Random left
                         for i=#available,1,-1 do
-                            if available[i]>=o.tracks*.5 then
+                            if available[i]>(o.tracks+1)*.5 then
                                 rem(available,i)
                             end
                         end
                     elseif c=='R'then--Random right
                         for i=#available,1,-1 do
-                            if available[i]<=o.tracks*.5 then
+                            if available[i]<(o.tracks+1)*.5 then
                                 rem(available,i)
                             end
                         end
@@ -213,17 +212,14 @@ function Map.new(file)
                         readState='time'
                         goto CONTINUE_nextState
                     end
-                    if #available>0 then
-                        curTrack=available[rnd(#available)]
-                        ins(o.eventQueue,{
-                            type="note",
-                            time=curTime,
-                            track=curTrack,
-                        })
-                        curLineState[curTrack]=true
-                    else
-                        error('[No available position to place notes] '..str0)
-                    end
+                    _syntaxCheck(#available>0,"No space to place notes")
+                    curTrack=available[rnd(#available)]
+                    ins(o.eventQueue,{
+                        type="note",
+                        time=curTime,
+                        track=curTrack,
+                    })
+                    trackUsed[curTrack]=true
                     str=str:sub(2)
                 elseif readState=='time'then
                     if str:sub(1,1)=='|'then--Shorten 1/2 for each
@@ -232,8 +228,8 @@ function Map.new(file)
                             str=str:sub(2)
                             if str==""then
                                 break
-                            elseif str:sub(1,1)~='|'then
-                                error("[Mixed mark] "..str0)
+                            else
+                                _syntaxCheck(str:sub(1,1)=='|',"Mixed mark")
                             end
                         end
                     elseif str:sub(1,1)=='~'then--Add 1 beat for each
@@ -242,29 +238,29 @@ function Map.new(file)
                             str=str:sub(2)
                             if str==""then
                                 break
-                            elseif str:sub(1,1)~='~'then
-                                error("[Mixed mark] "..str0)
+                            else
+                                _syntaxCheck(str:sub(1,1)=='~',"Mixed mark")
                             end
                         end
                     elseif str:sub(1,1)=='*'then--Multiply time by any number
                         local mul=tonumber(str:sub(2))
-                        assert(type(mul)=='number',"[Wrong scale num] "..str0)
+                        _syntaxCheck(type(mul)=='number',"Wrong scale num")
                         step=step*mul
                         break
                     elseif str:sub(1,1)=='/'then--Divide time by any number
                         local div=tonumber(str:sub(2))
-                        assert(type(div)=='number',"[Wrong scale num] "..str0)
+                        _syntaxCheck(type(div)=='number',"Wrong scale num")
                         step=step/div
                         break
                     elseif str==""then
                         break
                     else
-                        error("[Invalid time mark] "..str0)
+                        _syntaxCheck(false,"Invalid time mark")
                     end
                 end
                 ::CONTINUE_nextState::
             end
-            lastLineState=curLineState
+            lastLineState=trackUsed
             curTime=curTime+60/curBPM*step
         end
         line=line-1
