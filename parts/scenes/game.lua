bd994c15f9f0ebedae4729c5dfb7a695b38b29c0
MrZ626 <1046101471@qq.com>
2021-12-18 18:36:22 +0800

    变速功能键速度调整

--- parts/scenes/game.lua
+++ parts/scenes/game.lua
@@ -173,8 +173,8 @@ end
 -- end
 
 function scene.update(dt)
-    if kbIsDown'lctrl'and kbIsDown('0','-','=')then
-        dt=dt*(kbIsDown'0'and .5 or kbIsDown'-'and 3 or 8)
+    if kbIsDown'lctrl'and kbIsDown('9','0','-','=')then
+        dt=dt*(kbIsDown'9'and .25 or kbIsDown'0'and .5 or kbIsDown'-'and 4 or 16)
         if time-dt-playSongTime>0 then
             BGM.seek(time-dt-playSongTime)
         end
