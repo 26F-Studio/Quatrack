67ec9a53bdae37f15ef7ea8e76359a2ec6b28aeb
MrZ626 <1046101471@qq.com>
2022-01-02 23:39:27 +0800

    框架跟进

--- Zframework/mathExtend.lua
+++ Zframework/mathExtend.lua
@@ -1,5 +1,6 @@
 local MATH={}for k,v in next,math do MATH[k]=v end
 
+local int,ceil=math.floor,math.ceil
 local rnd=math.random
 
 MATH.tau=2*math.pi
@@ -27,6 +28,18 @@ function MATH.interval(v,low,high)
         return high
     else
         return v
+    end
+end
+
+function MATH.lerp(s,e,t)
+    return s+(e-s)*t
+end
+
+do--function MATH.listLerp(list,t)
+    local interval,lerp=MATH.interval,MATH.lerp
+    function MATH.listLerp(list,t)
+        local t2=(#list-1)*interval(t,0,1)+1
+        return lerp(list[int(t2)],list[ceil(t2)],t2%1)
     end
 end
 
