69f0283fcb00bf73007fae7f08c723aac553771f
MrZ_26 <1046101471@qq.com>
2022-01-15 16:57:30 +0800

    修复bassslut的脚本小问题

--- parts/levels/Bass Slut/script.lua
+++ parts/levels/Bass Slut/script.lua
@@ -19,6 +19,6 @@ end
 
 function drawBack()
     local t=game.time
-    if t>71.412 and t<72.412 then drawRectWave(t-71.412,32,0.3) end
-    if t>115.297 and t<116.297 then drawRectWave(t-115.297,32,0.3) end
+    if t>71.412 and t<76.412 then drawRectWave(t-71.412,32,0.3) end
+    if t>115.297 and t<120.297 then drawRectWave(t-115.297,32,0.3) end
 end
