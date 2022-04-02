b706e56c33709db710180ad162caf2c5b8af1c2a
MrZ_26 <1046101471@qq.com>
2022-04-03 03:01:12 +0800

    着色器技术飞跃 框架跟进

--- assets/background/aura.lua
+++ assets/background/aura.lua
@@ -1,23 +1,19 @@
--- Cool liquid background
+--Cool liquid background
 local gc=love.graphics
 local back={}
 local shader=SHADER.aura
 local t
 
 function back.init()
-    t=math.random()*2600
+    t=math.random()*260
     BG.resize(SCR.w,SCR.h)
 end
-function back.resize(_,h)
-    shader:send('w',SCR.W)
-    shader:send('h',h*SCR.dpi)
-end
 function back.update(dt)
-    t=t+dt
+    t=(t+dt)%2600
 end
 function back.draw()
     gc.clear(.08,.08,.084)
-    shader:send('t',t)
+    shader:send('phase',t)
     gc.setShader(shader)
     gc.rectangle('fill',0,0,SCR.w,SCR.h)
     gc.setShader()
